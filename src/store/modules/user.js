caf59774a5fcb57c0aaa3854204b0dbe926df56d
dengbowen <bowendeng@jieyuechina.com>
2018-09-27 17:53:51 +0800

    ddd

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -19,13 +19,25 @@ const user = {
             let themeLink = document.querySelector('link[name="theme"]');
             themeLink.setAttribute('href', '');
             // 清空打开的页面等数据，但是保存主题数据
-            let theme = '';
+            let theme = '', Jurisdiction = '', child='';
+            if (localStorage.Jurisdiction){
+                Jurisdiction = localStorage.Jurisdiction;
+            }
+            if (localStorage.child){
+                child = localStorage.child;
+            }
             if (localStorage.theme) {
                 theme = localStorage.theme;
             }
             localStorage.clear();
             if (theme) {
                 localStorage.theme = theme;
+            }
+            if (Jurisdiction) {
+                localStorage.Jurisdiction = Jurisdiction;
+            }
+            if (child) {
+                localStorage.child = child;
             }
         }
     }
